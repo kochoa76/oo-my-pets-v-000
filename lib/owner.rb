214cf092cce034eb0cadbb32889bd9d1c401637e
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-04 18:11:16 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,73 @@
 class Owner
   # code goes here
+  OWNERS=[]
+  attr_accessor :name, :pets
+  attr_reader :species 
+  
+  def initialize(name)
+    @name = name 
+    @pets = {fishes: [], cats: [], dogs: []}
+    @species = "human"
+    OWNERS<<self 
+  end 
+
+  def self.all
+   OWNERS
+  end 
+  
+  def self.reset_all 
+    OWNERS.clear
+  end
+  
+  def self.count 
+    OWNERS.size 
+  end 
+  
+  def say_species
+    "I am a human."
+  end 
+  
+  def buy_fish(name)
+   pet_fish= Fish.new(name)
+   @pets[:fishes]<<pet_fish 
+ end 
+ 
+  def buy_cat(name)
+   pet_cat = Cat.new(name)
+   @pets[:cats]<<pet_cat
+ end 
+ 
+  def buy_dog(name)
+   pet_dog = Dog.new(name)
+   @pets[:dogs]<<pet_dog
+ end 
+ 
+  def walk_dogs 
+    @pets[:dogs].each do |dogs|
+    dogs.mood="happy"
+  end
+end
+
+  def play_with_cats 
+  @pets[:cats].each do |cats|
+    cats.mood= "happy"
+  end 
+end 
+
+  def feed_fish 
+    @pets[:fish].each do |fishes|
+    fishes.mood="happy"
+  end 
+end 
+
+  
+
+  
+   
+    
+  
+  
+  
+
+
 end
